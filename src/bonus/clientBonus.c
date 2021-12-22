1d02f1d2e72e5f4f4e487f51d041c73dc2d546fd
Alessandro Pangrazio <apangraz@F3C030.42adel.org.au>
2021-12-22 13:57:53 +1030

    ready for submission

--- src/bonus/clientBonus.c
+++ src/bonus/clientBonus.c
@@ -1,30 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clientBonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apangraz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/22 13:55:22 by apangraz          #+#    #+#             */
+/*   Updated: 2021/12/22 13:55:47 by apangraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "miniTalk.h"
 
 static void	confirmHandler(int signum)
 {
 	(void)signum;
-	if(signum == SIGUSR1)
+	if (signum == SIGUSR1)
 	{
 		ft_printf("Message sent and recieved successfully!\n");
 		exit(0);
 	}
 }
 
-static void	sendMsg(int	serverPid, char byte)
+static void	sendMsg(int serverPid, char byte)
 {
 	int	bitShift;
 
 	bitShift = 0;
-	while(bitShift <= 7)
+	while (bitShift <= 7)
 	{
-		if((byte >> bitShift) & 1)
+		if ((byte >> bitShift) & 1)
 		{	
-			if(kill(serverPid, SIGUSR1) == -1)
+			if (kill(serverPid, SIGUSR1) == -1)
 				ft_printf("failed to send SIGUSR1 msg\n");
 		}		
 		else
 		{	
-			if(kill(serverPid, SIGUSR2) == -1)
+			if (kill(serverPid, SIGUSR2) == -1)
 				ft_printf("failed to send SIGUSR2 msg\n");
 		}
 		bitShift++;
@@ -32,34 +44,37 @@ static void	sendMsg(int	serverPid, char byte)
 	}
 }
 
-int	main(int argc, char **av)
+static int	argChecker(int counter)
 {
-	int					sPid;
-	struct sigaction	cntCiggie;
-	char				*msg;
-	int					i;
-
-	if(argc != 3)
+	if (counter != 3)
 	{
-		printf("CLIENT: invalid number of arguments\n");
+		ft_printf("CLIENT: Invalid number of arguments\n");
 		exit(1);
 	}
-	msg = av[2];
+	return (0);
+}
+
+int	main(int argc, char **av)
+{
+	int					sPid = ft_atoi(av[1]);
+	struct sigaction	cntCiggie;
+	char				*msg = av[2];
+	int					i = 0;
+
+	argChecker(argc);
 	msg[strlen(msg) + 1] = '\0';
 	cntCiggie.sa_handler = &confirmHandler;
-	sPid = ft_atoi(av[1]);
 	sigemptyset(&cntCiggie.sa_mask);
-	i = 0;
-	while(1)
+	while (1)
 	{	
 		sigaction(SIGUSR1, &cntCiggie, 0);
-		while(msg[i] != '\0')
+		while (msg[i] != '\0')
 		{
 			sendMsg(sPid, msg[i++]);
 		}
-		if(msg[i] == '\0')
+		if (msg[i] == '\0')
 			sendMsg(sPid, msg[i]);
 		pause();
 	}
-	return(0);
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -1,34 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serverBonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apangraz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/22 13:56:00 by apangraz          #+#    #+#             */
+/*   Updated: 2021/12/22 13:56:10 by apangraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "miniTalk.h"
 
 void	sigHandler(int signum, siginfo_t *siginfo, void *unused)
 {	
+	static unsigned char	bitValue = 0;
+	static int				bitShift = 0;
+
 	(void) unused;
-	static unsigned char bitValue = 0;
-	static int	bitShift = 0;
-	if(signum == SIGUSR1)
+	if (signum == SIGUSR1)
 		bitValue |= (1 << bitShift);
 	bitShift++;
-	if(bitShift == 8)
+	if (bitShift == 8)
 	{	
 		ft_printf("%c", bitValue);
-		if(!bitValue)
+		if (!bitValue)
 		{	
 			kill(siginfo->si_pid, SIGUSR1);
 		}	
 		bitValue = 0;
 		bitShift = 0;
-
 	}	
 }
 
 void	welcomeMsg(void)
 {
 	pid_t	pid;
+
 	pid = getpid();
-	ft_printf("\t\t========Server Up and Running========\n\t\t========Waiting for Client...========\n");
+	ft_printf("\t\t========Server Up and Running========\n");
+	ft_printf("\t\t========Waiting for Client...========\n");
 	ft_printf("\t\t\t Process ID is: %i\n", pid);
 	ft_printf("\t   ========To run use ./client PID ""mesage""========\n");
-
 }
 
 int	main(void)
@@ -38,14 +51,14 @@ int	main(void)
 	sigemptyset(&ciggie.sa_mask);
 	ciggie.sa_flags = SA_SIGINFO;
 	ciggie.sa_sigaction = &sigHandler;
-	welcomeMsg();	
-	while(1)
+	welcomeMsg();
+	while (1)
 	{
-		if((sigaction(SIGUSR1, &ciggie, 0)) == -1)
+		if ((sigaction(SIGUSR1, &ciggie, 0)) == -1)
 			ft_printf("SIGUSR1 Error\n");
-		if((sigaction(SIGUSR2, &ciggie, 0)) == -1)
+		if ((sigaction(SIGUSR2, &ciggie, 0)) == -1)
 			ft_printf("SIGUSR2 Error\n");
 		pause();
 	}
-	return(0);
+	return (0);
 }
